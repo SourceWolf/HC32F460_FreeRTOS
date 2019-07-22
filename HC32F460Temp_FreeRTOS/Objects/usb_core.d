43229270a23ddd17654b35a60d9ffd2ed4dd4a59
SourceWolf <snowwolf726@163.com>
2019-07-22 13:47:40 +0800

    delete Objects

--- HC32F460Temp_FreeRTOS/Objects/usb_core.d
+++
@@ -1,8 +0,0 @@
-.\objects\usb_core.o: usb_lib\ctl_drv\usb_core.c
-.\objects\usb_core.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\usb_core.o: usb_lib\ctl_drv\usb_core.h
-.\objects\usb_core.o: .\App\usb_conf.h
-.\objects\usb_core.o: usb_lib\ctl_drv\usb_otg_regs.h
-.\objects\usb_core.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\usb_core.o: usb_lib\ctl_drv\usb_defines.h
-.\objects\usb_core.o: .\App\usb_bsp.h
